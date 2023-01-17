283a3a40c40b3abbb66608912ee852bc68b85348
Jordy Marquez <jordymarquez@MacBook-Pro.local>
2023-01-17 10:50:28 -0800

    updated java

--- Develop/script.js
+++ Develop/script.js
@@ -9,12 +9,19 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+var saveBtnEl = ("saveBtn")
+  saveBtnEl.on("click", function () {
+  localStorage.setItem($(this).attr($('#body').children()));
+  console.log(saveBtnEl);
+})
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+  if 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
